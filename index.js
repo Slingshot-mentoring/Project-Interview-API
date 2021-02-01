53f8e3574c1265f453a5ad77e4b56db59c30b4f3
Aakarsh Singh <aakarshsingh0909@gmail.com>
2021-02-02 00:59:54 +0530

    brought up to date with local

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-var express = require("express");
-var app = express();
-var fs = require("fs");
+const express = require("express");
+const app = express();
+const fs = require("fs");
 const { spawn } = require("child_process");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+app.use(cors())
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+const PORT = process.env.PORT || 3001;
 app.get("/", function (req, res) {
   res.send("GET request to homepage");
 });
-app.get("/:code", function (req, res) {
-  fs.writeFile("temp.py", req.params.code, function (err) {
+app.post("/", function (req, res) {
+  let variables={
+    first:req.body.first,
+    second:req.body.second
+  }
+  fs.writeFile("temp.py", req.body.code, function (err) {
     if (err) throw err;
     console.log("Saved!");
   });
   var dataToSend;
   // spawn new child process to call the python script
-  const python = spawn("python", ["temp.py"]);
+  const python = spawn("python", ["temp.py",variables.first,variables.second]);
   // collect data from script
+  
   python.stdout.on("data", function (data) {
     console.log("Pipe data from python script ...");
     dataToSend = data.toString();
+    dataToSend=dataToSend.substring(0,dataToSend.length-1)
   });
   // in close event we are sure that stream from child process is closed
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
-    res.send(dataToSend);
+    res.json({output:dataToSend});
   });
 });
 
